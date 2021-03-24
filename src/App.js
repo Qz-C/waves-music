334dccf94485a81cc42de58860ec7246345e4d78
Ezequiel Fonseca <ezequiel_fs13@hotmail.com>
2021-03-24 20:57:36 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Player from "./components/Player";
+import Song from "./components/Song";
+import "./styles/app.scss";
+
+
+function App() {
+  return (
+    <div className="App">
+          <Song/>
+      <Player/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Player.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+    faPlay,
+    faAngleLeft,
+    faAngleRight
+} from "@fortawesome/free-solid-svg-icons";
+
+const Player = () => {
+    return (
+        <div className="player">
+            <div className="time-control">
+                <p>Start Time</p>
+                <input type="range" />
+                <p>End time</p>
+            </div>
+            <div className="play-control">
+                <FontAwesomeIcon className="skip-back" icon={faAngleLeft} size="2x" />
+                <FontAwesomeIcon className="play" icon={faPlay} size="2x"/>
+                <FontAwesomeIcon className="skip-forward" icon={faAngleRight} size="2x"/>
+            </div>
+        </div>
+    )
+}
+export default Player;
