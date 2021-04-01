42ea0a64856270da99394e51bb8eeb055b520d4d
Ezequiel Fonseca <ezequiel_fs13@hotmail.com>
2021-03-31 21:49:10 -0300

    update Player component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React , {useState} from "react";
+import React, { useState } from "react";
 import Player from "./components/Player";
 import Song from "./components/Song";
 import "./styles/app.scss";
@@ -9,11 +9,16 @@ function App() {
 
   const [songs, setSongs] = useState(data());
   const [currentSong, setCurrentSong] = useState(songs[0]);
+  const [isPlaying, setIsPlaying] = useState(false);
 
   return (
     <div className="App">
-          <Song currentSong={currentSong}/>
-      <Player/>
+      <Song currentSong={currentSong} />
+      <Player
+        currentSong={currentSong}
+        isPlaying={isPlaying}
+        setIsPlaying={setIsPlaying}
+      />
     </div>
   );
 }
--- src/components/Player.js
+++ src/components/Player.js
@@ -1,24 +1,70 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
     faPlay,
     faAngleLeft,
-    faAngleRight
+    faAngleRight,
+    faPause
 } from "@fortawesome/free-solid-svg-icons";
 
-const Player = () => {
+const Player = ({ currentSong, isPlaying, setIsPlaying }) => {
+
+    const audioRef = useRef(null);
+
+    const playSongHanler = () => {
+        if (isPlaying) {
+            audioRef.current.pause();
+            setIsPlaying(!isPlaying);
+        } else {
+            audioRef.current.play();
+            setIsPlaying(!isPlaying);
+        }
+    }
+
+    const getTime = (time) => {
+        return Math.floor(time / 60) + ":" + ("0" + Math.floor(time % 60)).slice(-2);
+    }
+
+    const timeUpdateHandler = (event) => {
+        const current = event.target.currentTime;
+        const duration = event.target.duration;
+        setSongInfo({ ...songInfo, currentTime: current, duration });
+    }
+
+    const dragHandler = (event) => {
+        audioRef.current.currentTime = event.target.value;
+        setSongInfo({ ...songInfo, currentTime: event.target.value });
+    }
+
+    const [songInfo, setSongInfo] = useState({
+        currentTime: 0,
+        duration: 0,
+    });
+
     return (
         <div className="player">
             <div className="time-control">
-                <p>Start Time</p>
-                <input type="range" />
-                <p>End time</p>
+                <p>{getTime(songInfo.currentTime)}</p>
+                <input
+                    onChange={dragHandler}
+                    min={0}
+                    max={songInfo.duration}
+                    value={songInfo.currentTime}
+                    type="range"
+                />
+                <p>{getTime(songInfo.duration)}</p>
             </div>
             <div className="play-control">
                 <FontAwesomeIcon className="skip-back" icon={faAngleLeft} size="2x" />
-                <FontAwesomeIcon className="play" icon={faPlay} size="2x"/>
-                <FontAwesomeIcon className="skip-forward" icon={faAngleRight} size="2x"/>
+                <FontAwesomeIcon className="play" onClick={playSongHanler} icon={isPlaying ? faPause : faPlay} size="2x" />
+                <FontAwesomeIcon className="skip-forward" icon={faAngleRight} size="2x" />
             </div>
+            <audio
+                onLoadedMetadata={timeUpdateHandler}
+                onTimeUpdate={timeUpdateHandler}
+                ref={audioRef}
+                src={currentSong.audio}>
+            </audio>
         </div>
     )
 }
