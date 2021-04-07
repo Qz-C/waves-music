d43c4df326d24207b53ba0f03fcf6666d4780191
Ezequiel Fonseca <ezequiel_fs13@hotmail.com>
2021-04-06 23:13:40 -0300

    add mobile style

--- src/App.js
+++ src/App.js
@@ -45,7 +45,7 @@ function App() {
   }
 
   return (
-    <div className="App">
+    <div className={`App ${libraryStatus ? "library-active": ""}`}>
       <Nav
         libraryStatus={libraryStatus}
         setLibraryStatus={setLibraryStatus}
